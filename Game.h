a502c395ee48215110637374c67c2cf9f32a54f5
Sauryadeep Pal <sauryadeep_pal@outlook.com>
2021-01-17 21:38:00 +0800

    Moved variable initialisation to class file. Changed some function signatures. Added getType().

--- Game.h
+++ Game.h
@@ -6,15 +6,15 @@ namespace Game {
         int row;
         int col;
         int type;
-        int points=0;
+        int points;
 
         public:
             Creature();
-            Creature(int a_row, int a_col, int a_type); 
+            Creature(int a_row, int a_col, int a_type);
+            int getType() const; 
             void getPosition(int& a_row, int& a_col); // get pacman position in maze and store in a_row, a_col
             void showCreaturePosition();
             void setPosition(int a_row, int a_col);
-            void updateCreaturePosition(int nrow, int ncol); // update based on new pos
             void addPoints(int a_point);
             void showCurrentPoints(); // show collected reward points
     };
@@ -33,7 +33,7 @@ namespace Game {
             void updateMaze(Creature p, int row_, int col_); // update maze based on location of pacman object p and its previous location
             bool isValidMove(int nrow, int ncol); // Check if the move to new location (nrow,ncol) is valid
             void placeCreatureInMaze(Creature p); // Position pacman object p into the maze
-            int getReward(Maze a_maze, int r, int c); // return points as reward if move into food mat[i][j]=2 at location (r,c)
+            int getReward(int r, int c); // return points as reward if move into food mat[i][j]=2 at location (r,c)
     };
 
     class CGame {
