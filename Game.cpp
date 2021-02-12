c42e19560bb86171f0120751be5b39e22ac9329f
Sauryadeep Pal <sauryadeep_pal@outlook.com>
2021-02-12 09:53:55 +0800

    Added code for Monster

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
 #include "Game.h"
 
 using namespace Game;
@@ -14,9 +15,11 @@ int Creature::getType() const {
     switch (type) {
         case 1:
             return 3;
+        case 2:
+            return 4;
         default:
             return type;
-    };
+    }
 }
 
 void Creature::getPosition(int& a_row, int& a_col) {
@@ -25,7 +28,8 @@ void Creature::getPosition(int& a_row, int& a_col) {
 }
 
 void Creature::showCreaturePosition() {
-    cout << "Pacman at position (" << row << ", " << col <<")" << endl;
+    string c_name = type == 1 ? "Pacman" : "Monster";
+    cout << c_name << " at position (" << row << ", " << col <<")" << endl;
 }
 
 void Creature::setPosition(int a_row, int a_col) {
@@ -39,6 +43,34 @@ void Creature::addPoints(int a_point) {
 
 void Creature::showCurrentPoints() {
     cout << "Your collected points = " << points << endl;
+}
+
+
+Monster::Monster(int row, int col) : Creature(row, col, 2) {
+    srand(time(0));
+}
+
+void Monster::getMove(int& next_row, int& next_col) {
+    int prev_row = row, prev_col = col, move = rand() % 3;
+    switch(move) {
+        case 0:
+            prev_row += 1;
+            break;
+        case 1:
+            prev_col += 1;
+            break;
+        case 2:
+            prev_row -= 1;
+            break;
+        case 3:
+            prev_col -= 1;
+            break;
+        default:
+            prev_row += 0;
+            prev_col += 0;
+    }
+    next_row = prev_row;
+    next_col = prev_col;
 }
 
 
@@ -107,6 +139,8 @@ void Maze::drawMaze() {
             case 3:
                 cout << "P";
                 break;
+            case 4:
+                cout << "M";
             default:
                 cout << "";
             }
@@ -120,7 +154,8 @@ void Maze::deleteMaze() {
     delete[] mat;
 }
 
-void Maze::updateMaze(Creature p, int row_, int col_) {
+template <class T>
+void Maze::updateMaze(T p, int row_, int col_) {
     int prevRow, prevCol;
     p.getPosition(prevRow, prevCol);
     mat[prevRow][prevCol] = 0;
@@ -145,31 +180,40 @@ int Maze::getReward(int r, int c) {
 }
 
 
-void CGame::checkMove(char move, Maze& a_maze , Creature& a_creature) {
-    int m_row, m_col;
-    a_creature.getPosition(m_row, m_col);
+CGame::CGame() : gameOver(false) {}
+
+void CGame::checkMove(char move, Maze& a_maze , Creature& a_creature, Monster& monster) {
+    int p_row, p_col, m_row, m_col;
+    a_creature.getPosition(p_row, p_col);
+    monster.getPosition(m_row, m_col);
     switch (move) {
         case 'w':
-            m_row -= 1;
+            p_row -= 1;
             break;
         case 'a':
-            m_col -= 1;
+            p_col -= 1;
             break;
         case 's':
-            m_row += 1;
+            p_row += 1;
             break;
         case 'd':
-            m_col += 1;
+            p_col += 1;
             break;
         case 'q':
             break;
         default:
             cout << "Invalid input." << endl << endl;
     }
-    if (a_maze.isValidMove(m_row, m_col)) {
-        a_creature.addPoints(a_maze.getReward(m_row, m_col));
-        a_maze.updateMaze(a_creature, m_row, m_col);
-        a_creature.setPosition(m_row, m_col);
+    if (a_maze.isValidMove(p_row, p_col)) {
+        a_creature.addPoints(a_maze.getReward(p_row, p_col));
+        a_maze.updateMaze(a_creature, p_row, p_col);
+        a_creature.setPosition(p_row, p_col);
+        do {
+            monster.getMove(m_row, m_col);
+        } while (!a_maze.isValidMove(m_row, m_col));
+        a_maze.updateMaze(monster, m_row, m_col);
+        monster.setPosition(m_row, m_col);
+        if (m_row == p_row && m_col == p_col) gameOver = true;
     } else {
         cout << "Invalid move." << endl << endl;
     }
