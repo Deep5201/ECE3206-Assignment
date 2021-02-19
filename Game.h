166809e8ab236aac81a4c74de410efbd25849487
Sauryadeep Pal <sauryadeep_pal@outlook.com>
2021-02-19 19:12:03 +0800

    Added new functions and variables

--- Game.h
+++ Game.h
@@ -30,6 +30,7 @@ namespace Game {
     class Maze {
         int numRow;
         int numCol;
+        int prevLoc; // stores the type of position the monster moves into (empty space or food)
         int** mat; // 2D matrix that stores the maze
         
         public:
@@ -38,17 +39,19 @@ namespace Game {
             void readMaze(); // read maze from text file and populate mat
             void drawMaze();
             void deleteMaze();
-            template <class T>
-            void updateMaze(T p, int row_, int col_); // update maze based on location of pacman object p and its previous location
+            void updateMaze(Creature& p, int row_, int col_); // update maze based on location of pacman object p and its previous location
             bool isValidMove(int nrow, int ncol); // Check if the move to new location (nrow,ncol) is valid
-            void placeCreatureInMaze(Creature p); // Position pacman object p into the maze
+            void placeCreatureInMaze(Creature& p); // Position pacman object p into the maze
             int getReward(int r, int c); // return points as reward if move into food mat[i][j]=2 at location (r,c)
+            int countFood(); // Check if there is any more food left in the maze
     };
 
     class CGame {
         public:
-            bool gameOver;
+            int gameOver;
             CGame();
+            void moveMonster(Maze& maze, Monster& monster); 
+            void checkGameOver(Maze& maze, Creature& creature, Monster& monster); // check if player monster has eaten player
             void checkMove(char move, Maze& a_maze , Creature& a_creature, Monster& monster); // check if the move by creature is valid or not
     };
 }
